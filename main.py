6136c5c020e8e61a7dec45e154e9ad702369615f
K Laxmi Narayan <59755666+klaxminarayan@users.noreply.github.com>
2020-07-04 21:07:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,58 @@
+import randomWordGenerator
+import printer
+
+vowels = ('a', 'e', 'i', 'o', 'u')
+
+
+def getUpdatedVisibleAlphabets(actualWord, visibleAlphabets, guess):
+    guessedAlphabets = ''
+
+    for i in range(len(actualWord)):
+        # If character is missing and actual character is equal to guessed character
+        guessedAlphabets += guess if visibleAlphabets[i] == '_' and actualWord[i] == guess else visibleAlphabets[i]
+
+    return guessedAlphabets
+
+
+def updateVisibleAlphabetsWithGuess(actualWord, guess, visibleAlphabets, attemptsRemaining):
+    if guess in actualWord:
+        visibleAlphabets = getUpdatedVisibleAlphabets(actualWord, visibleAlphabets, guess)
+    else:
+        attemptsRemaining -= 1
+    return visibleAlphabets, attemptsRemaining
+
+
+def gameHasEnded(actualWord, guessedWord, attemptsRemaining):
+    if guessedWord == actualWord:
+        return printer.success("You WON! :D")
+    elif attemptsRemaining <= 0:
+        printer.failure("You Lost :(")
+        return printer.information('Word was ' + actualWord)
+    return False
+
+
+def isVowel(character):
+    return character in vowels
+
+
+def createVisibleAlphabets(word):
+    return ''.join([character if isVowel(character) else '_' for character in word])
+
+
+def startGame(attempts=5):
+    actualWord = randomWordGenerator.getRandomWord()
+    visibleAlphabets = createVisibleAlphabets(actualWord)
+    printer.gameStatus(visibleAlphabets, attempts)
+
+    while True:
+        guess = input('Guess a character: ')
+        print()
+
+        visibleAlphabets, attempts = updateVisibleAlphabetsWithGuess(actualWord, guess, visibleAlphabets, attempts)
+        printer.gameStatus(visibleAlphabets, attempts)
+        if gameHasEnded(actualWord, visibleAlphabets, attempts):
+            break
+
+
+if __name__ == "__main__":
+    startGame()
---
+++ printer.py
@@ -0,0 +1,41 @@
+class Colors:
+    """ ANSI color codes """
+    BLACK = "\033[0;30m"
+    RED = "\033[0;31m"
+    GREEN = "\033[0;32m"
+    BLUE = "\033[0;34m"
+    PURPLE = "\033[0;35m"
+    YELLOW = "\033[1;33m"
+    BOLD = "\033[1m"
+    FAINT = "\033[2m"
+    ITALIC = "\033[3m"
+    UNDERLINE = "\033[4m"
+    BLINK = "\033[5m"
+    NEGATIVE = "\033[7m"
+    CROSSED = "\033[9m"
+    END = "\033[0m"
+
+
+def success(text='', end='\n' """ delimeter """):
+    print(Colors.GREEN + Colors.BOLD + text + Colors.END, end=end)
+    return True
+
+
+def failure(text='', end='\n'):
+    print(Colors.RED + Colors.BOLD + text + Colors.END, end=end)
+
+
+def information(text='', end='\n'):
+    print(Colors.BLUE + Colors.BOLD + text + Colors.END, end=end)
+    return True
+
+
+def warning(text='', end='\n'):
+    print(Colors.YELLOW + Colors.BOLD + text + Colors.END, end=end)
+
+
+def gameStatus(visibleAlphabets, attemptsRemaining):
+    information("Current State: ", end=" ")
+    for i in visibleAlphabets:
+        information(i, end=" ")
+    warning('\tAttempts Remaining : ' + str(attemptsRemaining))
